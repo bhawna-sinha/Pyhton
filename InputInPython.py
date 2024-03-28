d26ebf7599bedcef9df9e518dc89ac33d8b213fc
bhawna-sinha <114169817+bhawna-sinha@users.noreply.github.com>
2024-03-28 17:58:13 +0530

    Python Basics

---
+++ InputInPython.py
@@ -0,0 +1,15 @@
+#Taking input from user and printing it
+name = input("name :")
+print(name)
+
+age = int(input("age :"))
+print(age)
+
+price = float(input("price :"))
+print(price)
+
+name = input("name :")
+age = int(input("age :"))
+price = float(input("price :"))
+
+print("My name is", name,"and I am", age, "years old", price)
--- Python_learning.py
+++ Python_learning.py
@@ -3,7 +3,7 @@ print('Hello World')
 name = input('What is your name? ')
 print('Hello' + name)
 
-working with strings
+#working with strings
 
 first_name = 'Jamila'
 surname = 'smith'
@@ -16,7 +16,7 @@ print(len(first_name))
 print(len(surname))
 print(full_name.endswith('Smith'))
 
-working with numbers
+#working with numbers
 
 addition = 10 + 5
 subtraction = 10 - 5
@@ -30,14 +30,14 @@ print(multiplication)
 print(division)
 print(modulus)
 
-working with bolleans
+#working with bolleans
 
 print(10<=10)
 print(10>8)
 print(0==0)
 print(1>2)
 
-if statement
+#if statement
 
 is_adult = False
 age = 18
@@ -64,7 +64,7 @@ else:
     print('not an adult') 
 
 
-working with list
+#working with list
 
 cars= ['BMW', 'toyota', 'mercedes']
 print(len(cars))
@@ -74,7 +74,7 @@ print(cars[1])
 print(cars[2])
 
 
-working with loops
+#working with loops
 
 
 cars = ['bmw', 'toyota', 'mercedes']
@@ -88,129 +88,7 @@ for car in cars:
 else:
     print(car.capitalize())   
 
-working with while loop
-
-umber = 0
-
-while number <= 10:
-    print(number)
-
-
-number = 0
-
-while number <= 10:
-    print(number)
-    number = number + 1
-
-number = 0
-
-while number <= 10:
-    print(number)
-    number = number + 1
-else:
-    print('while loop ended and value of number is' + str(number))
-
-
-
-
-
-
-
-
-
-
-
-print('Hello World')
-
-name = input('What is your name? ')
-print('Hello' + name)
-
-working with strings
-
-first_name = 'Jamila'
-surname = 'smith'
-
-
-full_name = first_name +' ' + surname
-
-print(full_name)
-print(len(first_name))
-print(len(surname))
-print(full_name.endswith('Smith'))
-
-working with numbers
-
-addition = 10 + 5
-subtraction = 10 - 5
-division = 10 / 5
-multiplication = 10 * 5
-modulus = 10 % 4
-
-print(addition)
-print(subtraction)
-print(multiplication)
-print(division)
-print(modulus)
-
-working with bolleans
-
-print(10<=10)
-print(10>8)
-print(0==0)
-print(1>2)
-
-if statement
-
-is_adult = False
-age = 18
-
-if is_adult:
-    print("is adult")
-
-if age >= 18:
-    print('adult')  
-else:
-    print('not an adult') 
-
-
-
-is_adult = False
-age = 17
-
-if is_adult:
-    print("is adult")
-
-if age >= 18:
-    print('adult') 
-else: 
-    print('not an adult') 
-
-
-working with list
-
-cars= ['BMW', 'toyota', 'mercedes']
-print(len(cars))
-print(cars)
-print(cars[0])
-print(cars[1])
-print(cars[2])
-
-
-working with loops
-
-
-cars = ['bmw', 'toyota', 'mercedes']
-
-for car in cars:
-    print(car)
-
-for car in cars:
-    if car == 'bmw':
-        print(car.upper())
-else:
-    print(car.capitalize())   
-
-working with while loop
+#working with while loop
 
 umber = 0
 
