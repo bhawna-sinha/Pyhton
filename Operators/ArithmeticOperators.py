7acbeadc617fb9681330b0bd49417ebbbae2444f
bhawna-sinha <114169817+bhawna-sinha@users.noreply.github.com>
2024-04-10 09:50:10 +0530

    Fixed error

--- Operators/ArithmeticOperators.py
+++ Operators/ArithmeticOperators.py
@@ -9,3 +9,10 @@ print(a * b)
 print(a / b)
 print(a % b) # remainder
 print(a ** b) # a^b
+
+
+name = "Bhawna"
+skills = ["Power BI", "MS Excel", "SQL", "Python (currently learning)"]
+
+print("Name:", name)
+print(skills)
