99b072bd7e6dfbe4e3ec46b23bf1c4827966c4e2
bhawna-sinha <114169817+bhawna-sinha@users.noreply.github.com>
2024-06-20 11:14:59 +0530

    updated

--- Operators/AssignmentOperators.py
+++ Operators/AssignmentOperators.py
@@ -1,4 +1,16 @@
 #assignment operators
 num = 10
-num = num + 10 #10 + 10 => 20
-print("num : ", num)
+num += 10  #num1 = num + 10 #10 + 10 => 20
+print("num1 : ", num)
+
+"""num2 = num * 5
+print("num2 : ", num2)
+
+num3 = num / 2
+print("num3 : ", num3)
+
+num4 = num ** 4
+print("num4 : ", num)
+
+num5 = num % 2
+print("num5 : ", num5)"""
